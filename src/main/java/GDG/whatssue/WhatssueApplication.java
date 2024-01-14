f445bdb502865e65628ebdf3bf53035f48bcaf0f
Ji-minhyeok <gmh8257@gmail.com>
2024-01-14 21:03:18 +0900

    Initial commit

---
+++ src/main/java/GDG/whatssue/WhatssueApplication.java
@@ -0,0 +1,13 @@
+package GDG.whatssue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatssueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GDG/whatssue/WhatssueApplicationTests.java
@@ -0,0 +1,13 @@
+package GDG.whatssue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
